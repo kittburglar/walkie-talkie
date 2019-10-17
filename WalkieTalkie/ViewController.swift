23e57ebf3b2e314dbc8899b758ad35f76095a414
Kittiphong Xayasane <kxayasan@gmail.com>
2019-10-16 23:01:39 -0400

    Initial commit

---
+++ WalkieTalkie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WalkieTalkie
+//
+//  Created by Kittiphong Xayasane on 2019-10-16.
+//  Copyright © 2019 Kittiphong Xayasane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
